ef31d65e023b35fc943696b356fa9d775d982f3b
imtiaz zarif <imtiazzarif12@gmail.com>
2020-10-26 13:04:43 +0600

    fully responsive

---
+++ script.js
@@ -0,0 +1,14 @@
+$('a').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 0
+        },
+        1000
+      );
+    }
+  });
